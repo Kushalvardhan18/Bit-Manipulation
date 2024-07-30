79962842c44306b6a4d96712839e8fc705cdcfda
Kushal Vardhan <vardhank140102@gmail.com>
2024-07-30 11:26:29 +0530

    Update BitwiseOperators.java

--- BitwiseOperators.java
+++ BitwiseOperators.java
@@ -4,7 +4,7 @@ public class BitwiseOperators {
         System.out.println(5 | 6);  // Or Operator
         System.out.println(5 ^ 6);  // XOR Operator
         System.out.println(5 << 6); // Left Shift Operator  (a*2^b)
-        System.out.println(5 >> 6); // Right Shift Operator
+        System.out.println(5 >> 6); // Right Shift Operator (a/2^b)
         System.out.println(~6); // One's Complement
         System.out.println(6^8);
     }
