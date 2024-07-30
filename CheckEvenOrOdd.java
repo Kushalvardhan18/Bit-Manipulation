17875e39dcbc6b4ad702aaad6fdac0ee39991d63
Kushal Vardhan <vardhank140102@gmail.com>
2024-07-30 11:33:57 +0530

    Update CheckEvenOrOdd.java

--- CheckEvenOrOdd.java
+++ CheckEvenOrOdd.java
@@ -1,5 +1,18 @@
 public class CheckEvenOrOdd {
-    public static void main(String[] args){
+    public static void oddOrEven(int n){
+        int bitMask =1;
+        if((n& bitMask) ==0){
+            System.out.println("Even");
+        }if((n& bitMask) !=0){
+            System.out.println("Odd");
+        }
 
+    }
+    public static void main(String[] args){
+        // LSB =0 ----> Evem
+        // LSB =1 ----> Odd
+        oddOrEven(3);
+        oddOrEven(13);
+        oddOrEven(8);
     }
 }
