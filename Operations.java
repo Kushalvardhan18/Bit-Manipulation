6b3a15117ca7b2edad160114156d833d0adc882a
Kushal Vardhan <vardhank140102@gmail.com>
2024-07-31 10:34:01 +0530

    Update Operations.java

--- Operations.java
+++ Operations.java
@@ -19,17 +19,21 @@ public class Operations {
         return n & bitMask;
     }
     public static int updateIthBit(int n ,int i,int newBit){
-        if(newBit ==0){
-            return clearIthBit(n,i);
-        }
-        else{
-            return setIthBit(n,i);
-        }
+//        if(newBit ==0){
+//            return clearIthBit(n,i);
+//        }
+//        else{
+//            return setIthBit(n,i);
+//        }
+        n = clearIthBit(n,i);
+        int bitMask = newBit<<i;
+        return n | bitMask;
     }
     public static void main (String[] args){
         System.out.println(getIthBit(10,2));
         System.out.println(setIthBit(10,2));
         System.out.println(clearIthBit(10,1));
-        System.out.println(updateIthBit(10,1,0));
+        System.out.println(updateIthBit(10,2,1));
+
     }
 }
